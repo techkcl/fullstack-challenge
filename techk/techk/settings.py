ba413eb886e6288cbaa4ba8f2d9b4629f3e56a05
Carlos Veliz <cveliz@techk.cl>
2018-05-19 23:31:43 -0400

    Fix settings and add db.sqlite3 file to track changes

--- techk/techk/settings.py
+++ techk/techk/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'techk.apps.base',
-    'techk.apps.scraper',
+    'apps.base',
+    'apps.scraper',
 ]
 
 MIDDLEWARE = [
